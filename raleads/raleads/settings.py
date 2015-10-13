d5e0cd7498956f4ce6cfd1dfd2d9ffa4fc5714e2
Aaron Hampton <aaron@aaronhampton.com>
2015-10-13 03:31:34 +0000

    Changed USER_AGENT

--- raleads/raleads/settings.py
+++ raleads/raleads/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'raleads.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'raleads (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS=32
