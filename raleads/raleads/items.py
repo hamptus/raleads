ec89ec1b0735fc7f97fd3c7e34622b129cc93aba
Aaron Hampton <aaron@aaronhampton.com>
2015-10-13 04:27:32 +0000

    Initial commit of walmart spider

--- raleads/raleads/items.py
+++ raleads/raleads/items.py
@@ -8,7 +8,6 @@
 import scrapy
 
 
-class RaleadsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Product(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
--- raleads/raleads/spiders/walmart.py
+++ raleads/raleads/spiders/walmart.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
+import urlparse
+from w3lib.url import url_query_cleaner
+
+from raleads.items import Product
 
 
 class WalmartSpider(scrapy.Spider):
     name = "walmart"
-    allowed_domains = ["walmart.com"]
+    allowed_domains = ["www.walmart.com"]
     start_urls = (
         'http://www.walmart.com/',
     )
 
     def parse(self, response):
-        pass
+        # Get product details if /ip/ is in the URL
+        if '/ip/' in response.url:
+            # Remove the unnecessary parameters from the product url
+            clean_url = url_query_cleaner(response.url)
+            # Create a new Product
+            p = Product()
+            p['url'] = clean_url
+            p['title'] = response.xpath("//h1[@itemprop='name']/span/text()").extract()[0].strip()
+            yield p
+
+        # Check all of the links on the current page
+        for link in response.xpath("//a/@href").extract():
+            # Create an absolute url
+            abs_url = urlparse.urljoin(response.url, link.strip())
+            # Create a new request for a spider to crawl
+            yield Request(url=abs_url)
+
